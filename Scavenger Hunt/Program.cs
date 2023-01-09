f1479c16ee0c4213e9c3cc2755d4edd457bb40c3
HGutscher <harrisongutscher@icloud.com>
2023-01-09 10:26:20 -0600

    updated answers

--- Scavenger Hunt/Program.cs
+++ Scavenger Hunt/Program.cs
@@ -4,9 +4,12 @@ string input = "";
 int hint = 0;
 Console.WriteLine("Welcome to PTec scavenger hunt");
 Console.WriteLine("Go to https://trcm.ca on the Lets Talk Treaties tab for your information");
-Console.WriteLine("Question one: What treaty hand the lagest number of first nation communities?");
+Console.WriteLine("No uppercases");
+Console.WriteLine("Question one: What treaty had the lagest number of first nation communities?");
+Console.WriteLine("Ex: treaty # ");
 
-do {
+do
+{
     if (input != "")
     {
         Console.WriteLine("Wrong answer");
@@ -15,9 +18,9 @@ do {
         {
             Console.WriteLine("Hint: Numbered");
         }
-        input = Console.ReadLine();
     }
-}while (input != "Treaty 5" && input != "treaty 5");
+    input = Console.ReadLine();
+} while (input != "treaty 5");
 
 Console.WriteLine("Correct");
 Console.WriteLine("Question two: How does the Inniwak say [Scaed promises to one another]?");
@@ -35,12 +38,13 @@ do
         {
             Console.WriteLine("Hint: What is");
         }
-        input = Console.ReadLine();
     }
-} while (input != "Kitchi-astomatowin" && input != "kitchi-astomatowin");
+    input = Console.ReadLine();
+} while (input != "kitchi-astomatowin");
 
 Console.WriteLine("Correct");
 Console.WriteLine("Question three: What kind of document was the royal proclamation?");
+Console.WriteLine("Ex: a _________ document");
 
 hint = 0;
 input = "";
@@ -55,12 +59,12 @@ do
         {
             Console.WriteLine("Hint: Crown");
         }
-        input = Console.ReadLine();
     }
-} while (input != "A sweeping Document" && input != "a sweeping Document" && input != "Sweeping Document" && input != "sweeping Document");
+    input = Console.ReadLine();
+} while (input != "a sweeping document");
 
 Console.WriteLine("Correct");
-Console.WriteLine("Question four: First Nations belieed that the Treaties would __________ the transition to a new lifestyle that was taking hold at the of the 19th century.");
+Console.WriteLine("Question four: first Nations belieed that the Treaties would [__________] the transition to a new lifestyle that was taking hold at the of the 19th century.");
 
 hint = 0;
 input = "";
@@ -75,12 +79,13 @@ do
         {
             Console.WriteLine("Hint: Benefits");
         }
-        input = Console.ReadLine();
     }
-} while (input != "Facilitate" && input != "facilitate");
+    input = Console.ReadLine();
+} while (input != "facilitate");
 
 Console.WriteLine("Correct");
 Console.WriteLine("Question five: While Naton-to-Nation treaty making was done amoung leaders who else where intergral and valued in the process");
+Console.WriteLine("Ex: first Nations __________, __________, and __________");
 
 hint = 0;
 input = "";
@@ -95,17 +100,8 @@ do
         {
             Console.WriteLine("Hint: First");
         }
-        input = Console.ReadLine();
     }
-} while (input != "First Nations women, Elders  and community members" 
-    && input != "First Nations women, elders  and community members"
-    && input != "First nations women, elders  and community members"
-    && input != "first nations women, elders  and community members"
-    && input != "First Nations women Elders  and community members"
-    && input != "First Nations women elders  and community members"
-    && input != "First nations women elders  and community members"
-    && input != "first nations women elders  and community members"
-    );
-
+    input = Console.ReadLine();
+} while (input != "first nations women elders, and community members");
 Console.WriteLine("Correct");
-Console.WriteLine("Nice you done...");
+Console.WriteLine("Nice, you are done...");
